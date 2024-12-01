5d7bbb671c15977cb5dca24d54fca3284f20c0e6
Kai-Uwe Klavei <kklavei@gmx.de>
2024-12-01 16:02:45 +0100

    Day01 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,31 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputSize = input.size
+        val leftList = IntArray(inputSize)
+        val rightList = IntArray(inputSize)
+        for (i in input.indices) {
+            val pair: List<String> = input[i].split("   ")
+            leftList[i] = pair[0].toInt()
+            rightList[i] = pair[1].toInt()
+        }
+        leftList.sort()
+        rightList.sort()
+        var sum = 0
+        for (i in leftList.indices) {
+            sum += (leftList[i] - rightList[i]).absoluteValue
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
