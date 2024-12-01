a9852b0424eeafa3c662eb505275d15d5223af11
Kai-Uwe Klavei <kklavei@gmx.de>
2024-12-01 16:50:05 +0100

    Day01 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
 import kotlin.math.absoluteValue
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun readColumns(input: List<String>): List<IntArray> {
         val inputSize = input.size
-        val leftList = IntArray(inputSize)
-        val rightList = IntArray(inputSize)
+        val leftIntArray = IntArray(inputSize)
+        val rightIntArray = IntArray(inputSize)
         for (i in input.indices) {
             val pair: List<String> = input[i].split("   ")
-            leftList[i] = pair[0].toInt()
-            rightList[i] = pair[1].toInt()
+            leftIntArray[i] = pair[0].toInt()
+            rightIntArray[i] = pair[1].toInt()
         }
+        return listOf(leftIntArray, rightIntArray)
+    }
+
+    fun part1(input: List<String>): Int {
+        val columns = readColumns(input)
+        val leftList = columns[0]
+        val rightList = columns[1]
         leftList.sort()
         rightList.sort()
         var sum = 0
@@ -20,12 +27,21 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val columns = readColumns(input)
+        val leftList = columns[0]
+        val rightList = columns[1]
+        var sum = 0
+        for (i in leftList.indices) {
+            val c = rightList.count { it == leftList[i] }
+            sum += (leftList[i] * c)
+        }
+        return sum
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
